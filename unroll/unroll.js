cf2f2a3f257c8f01f2b4ff1d22f898c790e79769
Christian Wise <djnarcissist@Christians-MacBook-Pro.local>
2023-12-14 04:42:05 -0500

    Start the array

---
+++ unroll/unroll.js
@@ -0,0 +1,11 @@
+function unroll(squareArray) {
+    const result = [];
+    let top = 0;
+    let left = 0;
+    let buttom = squareArray.length -1; //last index in row 2
+    let right = squareArray[0].length - 1; // last index in row 2
+
+    while 
+}
+
+module.exports = unroll;
